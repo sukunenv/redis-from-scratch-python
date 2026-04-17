1a20d09df1d9bffc77116380057315cecbaeb81f
Kalify.dev <kalpin347@gmail.com>
2026-04-17 15:35:20 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
-
+    connection, _ = server_socket.accept()
 
 if __name__ == "__main__":
     main()
