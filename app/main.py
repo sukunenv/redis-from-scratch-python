2aa317b5761417ff730d2c8690d60a36586b045c
Kalify.dev <kalpin347@gmail.com>
2026-04-17 15:57:45 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,26 @@
-import socket  # noqa: F401
+import socket
+import threading
+
+
+def handle_client(connection):
+    try:
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(b"+PONG\r\n")
+    finally:
+        connection.close()
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment the code below to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() 
+
     while True:
-        data = connection.recv(1024)
-        if not data:
-            break
-        connection.sendall(b"+PONG\r\n")
+        connection, _ = server_socket.accept()
+        thread = threading.Thread(target=handle_client, args=(connection,))
+        thread.start()
+
 
 if __name__ == "__main__":
     main()
