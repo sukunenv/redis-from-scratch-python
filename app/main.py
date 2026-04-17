dd649e441654a18c31c733e69c54fa345a2ca7bc
Kalify.dev <kalpin347@gmail.com>
2026-04-17 15:42:16 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,8 +8,6 @@ def main():
     # Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-    
     connection, _ = server_socket.accept() 
     connection.sendall(b"+PONG\r\n")
 
