36b915f61ecbfe48307a0ec7d824d2c87db68c46
Kalify.dev <kalpin347@gmail.com>
2026-04-17 15:52:56 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,9 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() 
-    connection.sendall(b"+PONG\r\n")
-
-     while True:
+    while True:
         data = connection.recv(1024)
         if not data:
             break
